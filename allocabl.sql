6caca90a2897ea79b96dfc9314266df9bf694fab
ltiinagn <lingtianmsnexplorer@msn.com>
2020-03-23 02:30:53 +0800

    Updated database

--- allocabl.sql
+++ allocabl.sql
@@ -1,27 +1,19 @@
--- MySQL dump 10.13  Distrib 8.0.19, for Linux (x86_64)
+-- MySQL dump 10.13  Distrib 8.0.19, for Win64 (x86_64)
 --
--- Host: localhost    Database: allocabl
+-- Host: 127.0.0.1    Database: allocabl
 -- ------------------------------------------------------
 -- Server version	8.0.19
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!50503 SET NAMES utf8mb4 */;
+/*!50503 SET NAMES utf8 */;
 /*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
 /*!40103 SET TIME_ZONE='+00:00' */;
 /*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-
---
--- Current Database: `allocabl`
---
-
-CREATE DATABASE /*!32312 IF NOT EXISTS*/ `allocabl` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
-
-USE `allocabl`;
 
 --
 -- Table structure for table `agents`
@@ -46,29 +38,77 @@ CREATE TABLE `agents` (
 
 LOCK TABLES `agents` WRITE;
 /*!40000 ALTER TABLE `agents` DISABLE KEYS */;
-INSERT INTO `agents` VALUES ('5e440358e9f1273063695865','sales',1,2,NULL);
+INSERT INTO `agents` VALUES ('5e440358e9f1273063695865','sales',1,0,NULL),('5e64973ad8084c29e64ec4ae','finance',1,0,NULL),('5e649782d8084c29e64ec4b7','general',1,0,NULL);
 /*!40000 ALTER TABLE `agents` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
--- Table structure for table `waitlist`
+-- Table structure for table `waitlist_finance`
 --
 
-DROP TABLE IF EXISTS `waitlist`;
+DROP TABLE IF EXISTS `waitlist_finance`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `waitlist` (
-  `guestID` varchar(255) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+CREATE TABLE `waitlist_finance` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `socket_id` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `waitlist`
+-- Dumping data for table `waitlist_finance`
 --
 
-LOCK TABLES `waitlist` WRITE;
-/*!40000 ALTER TABLE `waitlist` DISABLE KEYS */;
-/*!40000 ALTER TABLE `waitlist` ENABLE KEYS */;
+LOCK TABLES `waitlist_finance` WRITE;
+/*!40000 ALTER TABLE `waitlist_finance` DISABLE KEYS */;
+/*!40000 ALTER TABLE `waitlist_finance` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `waitlist_general`
+--
+
+DROP TABLE IF EXISTS `waitlist_general`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `waitlist_general` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `socket_id` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `waitlist_general`
+--
+
+LOCK TABLES `waitlist_general` WRITE;
+/*!40000 ALTER TABLE `waitlist_general` DISABLE KEYS */;
+/*!40000 ALTER TABLE `waitlist_general` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `waitlist_sales`
+--
+
+DROP TABLE IF EXISTS `waitlist_sales`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `waitlist_sales` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `socket_id` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=32 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `waitlist_sales`
+--
+
+LOCK TABLES `waitlist_sales` WRITE;
+/*!40000 ALTER TABLE `waitlist_sales` DISABLE KEYS */;
+/*!40000 ALTER TABLE `waitlist_sales` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
@@ -80,4 +120,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2020-03-05 21:10:20
+-- Dump completed on 2020-03-23  2:29:35
