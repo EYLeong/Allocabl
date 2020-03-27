52b6a608ee255ffa24fc29cccfa7059b1a72ec8f
EYLeong <leong.enyi.1997@gmail.com>
2020-03-27 10:57:28 +0800

    added sql injection prevention for most database funcitons

--- helpers/databaseManager.js
+++ helpers/databaseManager.js
@@ -14,207 +14,198 @@ const setDatabase = dbName => {
 };
 
 function getAgent(department) {
+    let sql = `SELECT id FROM ${databaseName}.agents WHERE available = 1 AND department = ? ORDER BY customersServed`;
+    let inserts = [department];
+    sql = mysql.format(sql, inserts);
     return new Promise(function(resolve, reject) {
-        connectionPool.query(
-            `SELECT id FROM ${databaseName}.agents WHERE available = 1 AND department = '${department}' ORDER BY customersServed`,
-            function(err, rows) {
-                if (err) reject(err);
-                resolve(rows);
-            }
-        );
+        connectionPool.query(sql, function(err, rows) {
+            if (err) reject(err);
+            resolve(rows);
+        });
     });
 }
 
 const getAgentDepartment = agentID => {
+    let sql = `SELECT department FROM ${databaseName}.agents WHERE id = ?`;
+    let inserts = [agentID];
+    sql = mysql.format(sql, inserts);
     return new Promise((resolve, reject) => {
-        connectionPool.query(
-            `SELECT department FROM ${databaseName}.agents WHERE id = '${agentID}'`,
-            (err, result) => {
-                if (err) reject(err);
-                resolve(result);
-            }
-        );
+        connectionPool.query(sql, (err, result) => {
+            if (err) reject(err);
+            resolve(result);
+        });
     });
 };
 
 function getDepartment(socketID) {
+    let sql = `SELECT department FROM ${databaseName}.agents WHERE customerSocket = ?`;
+    let inserts = [socketID];
+    sql = mysql.format(sql, inserts);
     return new Promise(function(resolve, reject) {
-        connectionPool.query(
-            `SELECT department FROM ${databaseName}.agents WHERE customerSocket='${socketID}'`,
-            function(err, rows) {
-                if (err) reject(err);
-                resolve(rows);
-            }
-        );
+        connectionPool.query(sql, function(err, rows) {
+            if (err) reject(err);
+            resolve(rows);
+        });
     });
 }
 
 const incrementCustomersServed = agentID => {
+    let sql = `UPDATE ${databaseName}.agents SET customersServed = customersServed + 1 WHERE id = ?`;
+    let inserts = [agentID];
+    sql = mysql.format(sql, inserts);
     return new Promise((resolve, reject) => {
-        connectionPool.query(
-            `UPDATE ${databaseName}.agents SET customersServed = customersServed + 1 WHERE id = '${agentID}'`,
-            (err, result) => {
-                if (err) reject(err);
-                resolve(result);
-            }
-        );
+        connectionPool.query(sql, (err, result) => {
+            if (err) reject(err);
+            resolve(result);
+        });
     });
 };
 
 const addSocketAgent = (agentID, socketID) => {
+    let sql = `UPDATE ${databaseName}.agents SET customerSocket = ? WHERE id = ?`;
+    let inserts = [socketID, agentID];
+    sql = mysql.format(sql, inserts);
     return new Promise((resolve, reject) => {
-        connectionPool.query(
-            `UPDATE ${databaseName}.agents SET customerSocket = '${socketID}' WHERE id = '${agentID}'`,
-            (err, result) => {
-                if (err) reject(err);
-                resolve(result);
-            }
-        );
+        connectionPool.query(sql, (err, result) => {
+            if (err) reject(err);
+            resolve(result);
+        });
     });
 };
 
 const removeSocketAgent = socketID => {
+    let sql = `SET SQL_SAFE_UPDATES = 0;UPDATE ${databaseName}.agents SET available = 1, customerSocket = NULL WHERE customerSocket = ?;SET SQL_SAFE_UPDATES = 1`;
+    let inserts = [socketID];
+    sql = mysql.format(sql, inserts);
     return new Promise((resolve, reject) => {
-        connectionPool.query(
-            `SET SQL_SAFE_UPDATES = 0;UPDATE ${databaseName}.agents SET available = 1, customerSocket = NULL WHERE customerSocket = '${socketID}';SET SQL_SAFE_UPDATES = 1`,
-            (err, result) => {
-                if (err) reject(err);
-                resolve(result);
-            }
-        );
+        connectionPool.query(sql, (err, result) => {
+            if (err) reject(err);
+            resolve(result);
+        });
     });
 };
 
 const setAgentAvailable = agentID => {
+    let sql = `UPDATE ${databaseName}.agents SET available = 1 WHERE id = ?`;
+    let inserts = [agentID];
+    sql = mysql.format(sql, inserts);
     return new Promise((resolve, reject) => {
-        connectionPool.query(
-            `UPDATE ${databaseName}.agents SET available = 1 WHERE id = '${agentID}'`,
-            (err, result) => {
-                if (err) reject(err);
-                resolve(result);
-            }
-        );
+        connectionPool.query(sql, (err, result) => {
+            if (err) reject(err);
+            resolve(result);
+        });
     });
 };
 
 const setAgentUnavailable = agentID => {
+    let sql = `UPDATE ${databaseName}.agents SET available = 0 WHERE id = ?`;
+    let inserts = [agentID];
+    sql = mysql.format(sql, inserts);
     return new Promise((resolve, reject) => {
-        connectionPool.query(
-            `UPDATE ${databaseName}.agents SET available = 0 WHERE id = '${agentID}'`,
-            (err, result) => {
-                if (err) reject(err);
-                resolve(result);
-            }
-        );
+        connectionPool.query(sql, (err, result) => {
+            if (err) reject(err);
+            resolve(result);
+        });
     });
 };
 
 const setAgentOnline = agentID => {
+    let sql = `UPDATE ${databaseName}.agents SET online = 1 WHERE id = ?`;
+    let inserts = [agentID];
+    sql = mysql.format(sql, inserts);
     return new Promise((resolve, reject) => {
-        connectionPool.query(
-            `UPDATE ${databaseName}.agents SET online = 1 WHERE id = '${agentID}'`,
-            (err, result) => {
-                if (err) reject(err);
-                resolve(result);
-            }
-        );
+        connectionPool.query(sql, (err, result) => {
+            if (err) reject(err);
+            resolve(result);
+        });
     });
 };
 
 const setAgentOffline = agentID => {
+    let sql = `UPDATE ${databaseName}.agents SET online = 0 WHERE id = ?`;
+    let inserts = [agentID];
+    sql = mysql.format(sql, inserts);
     return new Promise((resolve, reject) => {
-        connectionPool.query(
-            `UPDATE ${databaseName}.agents SET online = 0 WHERE id = '${agentID}'`,
-            (err, result) => {
-                if (err) reject(err);
-                resolve(result);
-            }
-        );
+        connectionPool.query(sql, (err, result) => {
+            if (err) reject(err);
+            resolve(result);
+        });
     });
 };
 
 const addWaitList = (department, socketID) => {
+    let sql = `INSERT INTO ${databaseName}.waitlist_${department}(socket_id) VALUES(?)`;
+    let inserts = [socketID];
+    sql = mysql.format(sql, inserts);
     return new Promise((resolve, reject) => {
-        connectionPool.query(
-            `INSERT INTO ${databaseName}.waitlist_${department}(socket_id)
-            VALUES('${socketID}')`,
-            (err, result) => {
-                if (err) reject(err);
-                resolve(result);
-            }
-        );
+        connectionPool.query(sql, (err, result) => {
+            if (err) reject(err);
+            resolve(result);
+        });
     });
 };
 
 const getFromWaitList = department => {
+    let sql = `SELECT socket_id FROM ${databaseName}.waitlist_${department} ORDER BY id asc LIMIT 1`;
     return new Promise((resolve, reject) => {
-        connectionPool.query(
-            `SELECT socket_id FROM ${databaseName}.waitlist_${department} ORDER BY id asc LIMIT 1`,
-            (err, nextInList) => {
-                if (err) reject(err);
-                resolve(nextInList);
-            }
-        );
+        connectionPool.query(sql, (err, nextInList) => {
+            if (err) reject(err);
+            resolve(nextInList);
+        });
     });
 };
 
 const removeFromWaitList = department => {
+    let sql = `DELETE FROM ${databaseName}.waitlist_${department} ORDER BY id asc LIMIT 1`;
     return new Promise((resolve, reject) => {
-        connectionPool.query(
-            `DELETE FROM ${databaseName}.waitlist_${department} ORDER BY id asc LIMIT 1`,
-            (err, dump) => {
-                if (err) reject(err);
-                resolve(dump);
-            }
-        );
+        connectionPool.query(sql, (err, dump) => {
+            if (err) reject(err);
+            resolve(dump);
+        });
     });
 };
 
 const removeFromAllWaitlistsById = socketID => {
+    let sql = `SET SQL_SAFE_UPDATES = 0;DELETE FROM ${databaseName}.waitlist_sales WHERE socket_id = ?;DELETE FROM ${databaseName}.waitlist_finance WHERE socket_id = ?;DELETE FROM ${databaseName}.waitlist_general WHERE socket_id = ?;SET SQL_SAFE_UPDATES = 1`;
+    let inserts = [socketID, socketID, socketID];
+    sql = mysql.format(sql, inserts);
     return new Promise((resolve, reject) => {
-        connectionPool.query(
-            `SET SQL_SAFE_UPDATES = 0;DELETE FROM ${databaseName}.waitlist_sales WHERE socket_id = '${socketID}';DELETE FROM ${databaseName}.waitlist_finance WHERE socket_id = '${socketID}';DELETE FROM ${databaseName}.waitlist_general WHERE socket_id = '${socketID}';SET SQL_SAFE_UPDATES = 1`,
-            (err, dump) => {
-                if (err) reject(err);
-                resolve(dump);
-            }
-        );
+        connectionPool.query(sql, (err, dump) => {
+            if (err) reject(err);
+            resolve(dump);
+        });
     });
 };
 
 const checkDepartmentOnline = department => {
+    let sql = `SELECT id FROM ${databaseName}.agents WHERE online = 1 AND department = ? ORDER BY customersServed`;
+    let inserts = [department];
+    sql = mysql.format(sql, inserts);
     return new Promise(function(resolve, reject) {
-        connectionPool.query(
-            `SELECT id FROM ${databaseName}.agents WHERE online = 1 AND department = '${department}' ORDER BY customersServed`,
-            function(err, rows) {
-                if (err) reject(err);
-                resolve(rows);
-            }
-        );
+        connectionPool.query(sql, function(err, rows) {
+            if (err) reject(err);
+            resolve(rows);
+        });
     });
 };
 
 const clearDepartmentWaitlist = department => {
+    let sql = `SET SQL_SAFE_UPDATES = 0;DELETE FROM ${databaseName}.waitlist_${department};SET SQL_SAFE_UPDATES = 1`;
     return new Promise(function(resolve, reject) {
-        connectionPool.query(
-            `SET SQL_SAFE_UPDATES = 0;DELETE FROM ${databaseName}.waitlist_${department};SET SQL_SAFE_UPDATES = 1`,
-            function(err, rows) {
-                if (err) reject(err);
-                resolve(rows);
-            }
-        );
+        connectionPool.query(sql, function(err, rows) {
+            if (err) reject(err);
+            resolve(rows);
+        });
     });
 };
 
 const getDepartmentWaitlist = department => {
+    let sql = `SELECT * FROM ${databaseName}.waitlist_${department}`;
     return new Promise(function(resolve, reject) {
-        connectionPool.query(
-            `SELECT * FROM ${databaseName}.waitlist_${department}`,
-            function(err, rows) {
-                if (err) reject(err);
-                resolve(rows);
-            }
-        );
+        connectionPool.query(sql, function(err, rows) {
+            if (err) reject(err);
+            resolve(rows);
+        });
     });
 };
 
